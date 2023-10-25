50a1e0d9f15079cb77b1508230932475e056d1b4
Chen Wang <99030776+wangc9@users.noreply.github.com>
2023-10-25 11:31:13 +0300

    Add e2e tests and change CI workflow

---
+++ cypress/e2e/pokedex.cy.js
@@ -0,0 +1,7 @@
+describe('Pokedex', () => {
+  it('Front page can be opened', () => {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
